fa5aaeb99026946e3f2912bc6a4b095a0bc3e022
Yassineachkhity <125568814+Yassineachkhity@users.noreply.github.com>
2024-02-09 12:59:46 +0100

    Add files via upload

---
+++ suite.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int debut(0);
+  do {
+    cout << "de (>= 1) ? ";
+    cin >> debut;
+  } while (debut < 1);
+
+  int fin(0);
+  do {
+    cout << "a (>= " << debut << ") ? ";
+    cin >> fin;
+  } while (fin < debut);
+
+  /*******************************************
+   * Completez le programme a partir d'ici.
+   *******************************************/
+  for(int i(debut); i <= fin; ++i){
+    int n(i);
+    int k (0);
+    while( n != 0 ){
+      if( n % 3 == 0){
+        n += 4;
+      } else if( n % 4 == 0){
+        n /= 2;
+      } else{
+        n--;
+      }
+      k++;
+    }
+    cout << i << " -> " << k << endl;
+  }
+
+  /*******************************************
+   * Ne rien modifier apres cette ligne.
+   *******************************************/
+
+  return 0;
+}
